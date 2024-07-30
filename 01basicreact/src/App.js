4a6b421113537fed807b862f7d8dc256283d48a9
rehan0189 <hacksandhu4862@gmail.com>
2024-07-30 19:11:53 +0500

    Two Project of react just created one simple and second with vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div>
+      <h1>Rehan Sandhu</h1>
+    </div>
+  );
+}
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1> Rehan Sandhu vite React</h1>
+    </>
+  )
+}
+
+export default App
